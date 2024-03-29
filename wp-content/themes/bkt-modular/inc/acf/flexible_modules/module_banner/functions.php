7f652f8abf7c62c57a7073b3cde963f1aa6b0e4c
Juan Mayor <fmayor@brooktec.com>
2024-03-29 16:33:17 +0100

    Add module banner

--- wp-content/themes/bkt-modular/inc/acf/flexible_modules/module_banner/functions.php
+++ wp-content/themes/bkt-modular/inc/acf/flexible_modules/module_banner/functions.php
@@ -2,33 +2,36 @@
 
 function bkt_flex_module_banner_content () {
     $module_name = basename( __DIR__ );
-    $image = get_sub_field($module_name.'_image');
+    $bg_image = get_sub_field($module_name.'_bg_image');
     $title = get_sub_field($module_name.'_title');
     $subtitle = get_sub_field($module_name.'_subtitle');
+    $text = get_sub_field($module_name.'_text');
     $link = get_sub_field($module_name.'_btn_link');
+    $btn_type = get_sub_field($module_name.'_btn_type');
     $bg_color = get_sub_field($module_name.'_bgcolor');
     ?>
         <section class="mod-banner <?php echo $bg_color; ?>">
-            <div class="container">
-                <div class="mod-content row no-gutters align-items-center">
-                    <div class="col-md-6">
-                        <div class="container text-wrapper text-center">
-                            <h2 class="small-title text-center"><?php echo $title; ?></h2>
-                            <h3 class="big-title text-center"><?php echo $subtitle; ?></h3>
-                            <?php
-                                if ($link['url']) :
-                                    ?>
-                                        <a target="<?php echo $link['target']; ?>" class="btn white-btn" href=<?php echo $link['url']; ?>>
-                                            <?php echo ( $link['title'] ) ? $link['title'] : __('Ver más', BKT_MODULAR_THEME_LANG_DOMINE); ?>
-                                        </a>
-                                    <?php
-                                endif;
-                            ?>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="image-wrapper" style="background-image: url('<?php echo $image;?>');">
-                        </div>
+            <div class="content">
+                <div class="image-wrapper" style="background-image: url('<?php echo $bg_image;?>');">
+                    <div class="container text-wrapper">
+                        <?php if($title) :?>
+                            <h2 class="small-title"><?php echo $title; ?></h2>
+                        <?php endif; ?>
+                        <?php if($subtitle) :?>
+                            <h3 class="big-title"><?php echo $subtitle; ?></h3>
+                        <?php endif; ?>
+                        <?php if($text): ?>
+                            <div class="text"><?php echo $text; ?></div>
+                        <?php endif; ?>
+                        <?php
+                            if ($link['url']) :
+                                ?>
+                                    <a target="<?php echo $link['target']; ?>" class="<?php echo $btn_type; ?>" href=<?php echo $link['url']; ?>>
+                                        <?php echo ( $link['title'] ) ? $link['title'] : __('Ver más', BKT_MODULAR_THEME_LANG_DOMINE); ?>
+                                    </a>
+                                <?php
+                            endif;
+                        ?>
                     </div>
                 </div>
             </div>
